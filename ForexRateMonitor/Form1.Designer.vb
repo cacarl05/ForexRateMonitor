df8613ae109e67f42d0e60255559d665115395e8
cacarl05 <128402055+cacarl05@users.noreply.github.com>
2025-06-24 18:31:52 +0800

    Add files via upload

---
+++ ForexRateMonitor/Form1.Designer.vb
@@ -0,0 +1,93 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        lblRate = New Label()
+        lblStatus = New Label()
+        btnRefresh = New Button()
+        btnBackup = New Button()
+        Timer1 = New Timer(components)
+        SuspendLayout()
+        ' 
+        ' lblRate
+        ' 
+        lblRate.AutoSize = True
+        lblRate.Location = New Point(58, 97)
+        lblRate.Name = "lblRate"
+        lblRate.Size = New Size(55, 15)
+        lblRate.TabIndex = 0
+        lblRate.Text = "USD/EUR"
+        ' 
+        ' lblStatus
+        ' 
+        lblStatus.AutoSize = True
+        lblStatus.Location = New Point(58, 139)
+        lblStatus.Name = "lblStatus"
+        lblStatus.Size = New Size(48, 15)
+        lblStatus.TabIndex = 1
+        lblStatus.Text = "Status..."
+        ' 
+        ' btnRefresh
+        ' 
+        btnRefresh.Location = New Point(58, 245)
+        btnRefresh.Name = "btnRefresh"
+        btnRefresh.Size = New Size(128, 23)
+        btnRefresh.TabIndex = 2
+        btnRefresh.Text = "Refresh Rate"
+        btnRefresh.UseVisualStyleBackColor = True
+        ' 
+        ' btnBackup
+        ' 
+        btnBackup.Location = New Point(227, 245)
+        btnBackup.Name = "btnBackup"
+        btnBackup.Size = New Size(152, 23)
+        btnBackup.TabIndex = 3
+        btnBackup.Text = "Backup Database"
+        btnBackup.UseVisualStyleBackColor = True
+        ' 
+        ' Timer1
+        ' 
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(btnBackup)
+        Controls.Add(btnRefresh)
+        Controls.Add(lblStatus)
+        Controls.Add(lblRate)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents lblRate As Label
+    Friend WithEvents lblStatus As Label
+    Friend WithEvents btnRefresh As Button
+    Friend WithEvents btnBackup As Button
+    Friend WithEvents Timer1 As Timer
+
+End Class
---
+++ ForexRateMonitor/Form1.vb
@@ -0,0 +1,107 @@
+﻿Imports System.Net.Http
+Imports System.Windows.Forms
+Imports MySql.Data.MySqlClient
+Imports System.IO
+
+Public Class Form1
+    Private apiUrl As String = "https://api.frankfurter.app/latest?from=USD&to=EUR"
+    Private connectionString As String = "server=localhost;user id=root;password=;database=forexdb;"
+    Private backupFilePath As String = "C:\backups\forex_backup.sql"
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Interval = 60000 ' Refresh every minute
+        Timer1.Start()
+        If lblStatus IsNot Nothing Then
+            lblStatus.Text = "Monitoring started."
+        End If
+    End Sub
+
+    Private Async Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Await FetchExchangeRate()
+    End Sub
+
+    Private Async Function FetchExchangeRate() As Task
+        Try
+            Using client As New HttpClient()
+                Dim response = Await client.GetStringAsync(apiUrl)
+                MessageBox.Show(response) ' Shows API result for debugging
+
+                Dim rates = Newtonsoft.Json.JsonConvert.DeserializeObject(Of FrankfurterResponse)(response)
+
+                If rates IsNot Nothing AndAlso rates.rates.ContainsKey("EUR") Then
+                    Dim exchangeRate = rates.rates("EUR")
+
+                    If lblRate IsNot Nothing Then
+                        lblRate.Text = String.Format("USD/EUR: {0:F4}", exchangeRate)
+                    End If
+
+                    If exchangeRate > 1.2D Then
+                        MessageBox.Show("Rate is above threshold!")
+                    End If
+
+                    LogRateToDatabase("USD", "EUR", exchangeRate)
+
+                    If lblStatus IsNot Nothing Then
+                        lblStatus.Text = "Last updated: " & DateTime.Now.ToString()
+                    End If
+                Else
+                    MessageBox.Show("Error: EUR rate not found in response.")
+                End If
+            End Using
+        Catch ex As Exception
+            MessageBox.Show("An error occurred: " & ex.Message)
+        End Try
+    End Function
+
+    Private Sub LogRateToDatabase(currency1 As String, currency2 As String, rate As Decimal)
+        Try
+            Using connection As New MySqlConnection(connectionString)
+                connection.Open()
+                Dim command As New MySqlCommand("INSERT INTO rates (currency1, currency2, rate, timestamp) VALUES (@currency1, @currency2, @rate, @timestamp)", connection)
+                command.Parameters.AddWithValue("@currency1", currency1)
+                command.Parameters.AddWithValue("@currency2", currency2)
+                command.Parameters.AddWithValue("@rate", rate)
+                command.Parameters.AddWithValue("@timestamp", DateTime.Now)
+                command.ExecuteNonQuery()
+            End Using
+        Catch ex As Exception
+            MessageBox.Show("Database error: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub BackupDatabase()
+        Try
+            If Not Directory.Exists("C:\backups") Then
+                Directory.CreateDirectory("C:\backups")
+            End If
+            Dim backupCommand As String = "mysqldump -u root -pyourpassword forexdb > """ & backupFilePath & """"
+            Dim process As New Process()
+            process.StartInfo.FileName = "cmd.exe"
+            process.StartInfo.Arguments = "/C " & backupCommand
+            process.StartInfo.RedirectStandardOutput = True
+            process.StartInfo.UseShellExecute = False
+            process.StartInfo.CreateNoWindow = True
+            process.Start()
+            process.WaitForExit()
+            MessageBox.Show("Database backup completed successfully.")
+        Catch ex As Exception
+            MessageBox.Show("Backup error: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub btnBackup_Click(sender As Object, e As EventArgs) Handles btnBackup.Click
+        BackupDatabase()
+    End Sub
+
+    Private Async Sub btnRefresh_Click(sender As Object, e As EventArgs) Handles btnRefresh.Click
+        Await FetchExchangeRate()
+    End Sub
+
+    Private Sub lblRate_Click(sender As Object, e As EventArgs)
+        ' Do nothing
+    End Sub
+End Class
+
+Public Class FrankfurterResponse
+    Public Property rates As Dictionary(Of String, Decimal)
+End Class
